819d9c60eb5675e5c19f9e5caae7f8ec21731fed
Kura <kura@kura.io>
2014-07-24 23:56:02 +0100

    Forgot to wrap a string

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,7 @@ html_theme_options = {}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = PyPI Shields/Pins
+html_title = "PyPI Shields/Pins"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
