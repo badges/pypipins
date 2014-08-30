5001232c96cc35af34f362e6b2ce97d7f142273c
Kura <kura@kura.io>
2014-08-30 22:08:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from setuptools import setup, find_packages
 setup(
     name='pypipins',
     version='0.0.0',
-    description='Badges for your site to display download totals, latest version using crate.io https://pypip.in',
+    description='Badges for your site to display download totals, latest version using PyPI https://pypip.in',
     # long_description=long_description,
     author='Kura',
     author_email='kura@kura.io',
     url='https://github.com/badges/pypipins',
     packages=find_packages(),
     zip_safe=True,
-    keywords='PyPI crate.io badges pins',
+    keywords='PyPI badges/pins/shields',
     install_requires=[
-        'Twisted>=13.2.0',
-        'klein>=0.2.3',
-        'requests>=2.2.1',
-        'yarg>=0.1.9',
-        'redis>=2.10.3'
+        'Twisted',
+        'klein',
+        'requests',
+        'yarg',
+        'redis'
     ],
     classifiers=[
         'Programming Language :: Python :: 2',
